c8239a9b1e17083fcf8313850b4d47f849d3af2d
haggy24 <muhammadhaggy@gmail.com>
2022-09-10 12:39:27 +0700

    successfully deployed on local server

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def show_katalog(request):
+    data_catalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_catalog,
+        'student_id': '2106750370',
+        'nama': 'Muhammad Haggy'
+    }
+    return render(request, 'katalog.html', context)
