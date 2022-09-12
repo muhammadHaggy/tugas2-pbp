c167e6378ffebe43ccc2c7afecc547cfa00dc5f3
haggy24 <muhammadhaggy@gmail.com>
2022-09-12 08:23:13 +0700

    add test for katalog url and template

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class KatalogViewTest(TestCase):
+    def test_view_url_exist(self):
+        response = self.client.get('/katalog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get('/katalog/')
+        self.assertTemplateUsed(response, 'katalog.html')
