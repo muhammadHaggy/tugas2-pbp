895ab29baa5a91cdf6d007346a5ef6125bd25737
Muhammad Haggy <86955979+muhammadHaggy@users.noreply.github.com>
2022-09-16 07:52:06 +0700

    add testing for url and response code in mywatchlist app

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+class WatchlistViewTest(TestCase):
+    def test_html_url_exist(self):
+        response = self.client.get('/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_json_url_exist(self):
+        response = self.client.get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_xml_url_exist(self):
+        response = self.client.get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
